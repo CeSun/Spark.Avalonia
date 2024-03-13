4e8fc358fdaa59f862a9c87d07c318a2f08cc9b2
CeSun <ce.sun@outlook.com>
2024-03-14 01:55:37 +0800

    添加项目文件。

---
+++ Source/Example/Example/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Example.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Source/Spark.Avalonia/SparkCanvas.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.OpenGL;
+using Avalonia.OpenGL.Controls;
+using Silk.NET.OpenGLES;
+using System.Diagnostics;
+
+namespace Spark.Avalonia;
+
+public class SparkCanvas : OpenGlControlBase
+{
+    Engine engine = new Engine();
+    Stopwatch deltaTimeStopwatch = new Stopwatch();
+
+    protected override void OnOpenGlInit(GlInterface gl)
+    {
+        base.OnOpenGlInit(gl);
+        deltaTimeStopwatch.Start();
+    }
+    protected override void OnOpenGlRender(GlInterface gl, int fb)
+    {
+        deltaTimeStopwatch.Stop();
+        float deltaTime = (float)deltaTimeStopwatch.Elapsed.TotalSeconds;
+        deltaTimeStopwatch.Restart();
+        engine.Update(deltaTime);
+        engine.Render(GL.GetApi(gl.GetProcAddress));
+        RequestNextFrameRendering();
+    }
+
+    protected override void OnOpenGlDeinit(GlInterface gl)
+    {
+        base.OnOpenGlDeinit(gl);
+    }
+}
